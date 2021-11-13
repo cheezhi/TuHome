f1a174b078e0e4b153a119a531c123f4c9b1a791
tutu <2093142951@qq.com>
2021-11-13 13:45:10 +0800

    TuHome

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2021 TuHome</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
