03dca596b3216bbbf81268c9e00a9fed273308ad
ye土土 <53083230+ye-tutu@users.noreply.github.com>
2022-03-27 04:56:27 +0800

    Update footer.js

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2021 TuHome</p>\n' +
+    '<p style="color:#333;">© 2021-2022 TuHome</p>\n' +
     '</center>\n' +
     '');
 
