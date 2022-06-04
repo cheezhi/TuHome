a2c9b0e6f9e88ac4df559e7055a2b7523603c198
Tutu <53083230+ye-tutu@users.noreply.github.com>
2022-06-04 18:10:16 +0800

    Update header.js

--- source/js/header.js
+++ source/js/header.js
@@ -1,6 +1,6 @@
 document.write('<!-----------  公共js和css start----------->');
 document.write('' +
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bynotes/texiao/source/css/shubiao.css">\n' +
+    '<link rel="stylesheet" href="https://jsd.licaoz.com/gh/bynotes/texiao/source/css/shubiao.css">\n' +
     '<link rel="stylesheet" href="https://at.alicdn.com/t/font_2037988_gjhbcpzhs67.css" type="text/css">\n' +
 //     '<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?8c0854c37b23b617c441acb65529fca1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>\n' +
     '<link rel="apple-touch-icon" sizes="76x76" href="source/img/logo.png">\n' +
